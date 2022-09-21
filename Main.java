a8ede4c4640679f9854c42e32d8a285f6c9c23c4
JesusRO0 <78034035+JesusRO0@users.noreply.github.com>
2022-09-21 19:31:28 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+package Actividad9;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        persona[] persona = new persona[2];
+        persona persona1 = new persona();
+  
+
+
+    }
+    
+}
