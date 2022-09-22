8dad6046b813ead93435d55f2ee395edd2dc7c26
JesusRO0 <6193@iespuertasdelcampo.es>
2022-09-22 12:17:26 +0200

    3 persona

--- Actividad9/Main.java
+++ Actividad9/Main.java
@@ -4,9 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
 
-        persona[] persona = new persona[2];
+        persona[] persona = new persona[3];
         persona persona1 = new persona();
         persona persona2 = new persona();
+        persona persona3 = new persona();
     }
     
 }
