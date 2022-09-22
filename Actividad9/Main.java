244693fa826ee1a7cec1593d2c9cd920ab0dc59c
JesusRO0 <6193@iespuertasdelcampo.es>
2022-09-22 12:40:35 +0200

    Actividad9

--- Actividad9/Main.java
+++ Actividad9/Main.java
@@ -5,11 +5,21 @@ public class Main {
     public static void main(String[] args) {
 
         persona[] persona = new persona[4];
-        persona persona1 = new persona();
-        persona persona2 = new persona();
-        persona persona3 = new persona();
-        persona persona4 = new persona();
-        
+        persona persona1 = new persona("45159890A","Pérez López","Juan","Hombre",25,75.9);
+        persona persona2 = new persona("45159890C","Rodriguez","Pedro","Hombre",29,60.2);
+        persona persona3 = new persona("45159890D","Guitierrez","Laura","Mujer",18,80.5);
+        persona persona4 = new persona("45159890F","Ramirez","Cristina","Mujer",20,72.1);
+
+        persona[0] = persona1;
+        persona[1] = persona2;
+        persona[2] = persona3;
+        persona[3] = persona4;
+
     }
     
+
+    public void listarDni(){
+
+        
+    }
 }
